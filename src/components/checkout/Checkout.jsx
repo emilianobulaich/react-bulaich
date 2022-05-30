b91c2b6384ab34a3522bee95e91f03224f47e918
Emiliano <bulaich.emiliano@gmail.com>
2022-05-29 21:01:18 -0300

    tabla de productos

--- src/components/checkout/Checkout.jsx
+++ src/components/checkout/Checkout.jsx
@@ -1,6 +1,15 @@
 //@ts-check
-import React, { useState, useContext } from "react";
-import { Box, TextField, Button, Stack, Alert } from "@mui/material";
+import React, { useState, useContext, useEffect } from "react";
+import {
+  Box,
+  TextField,
+  Button,
+  Stack,
+  Alert,
+  Paper,
+  Backdrop,
+  CircularProgress,
+} from "@mui/material";
 import { NavLink } from "react-router-dom";
 import {
   collection,
@@ -19,15 +28,19 @@ export default function Checkout() {
     email: "",
     phone: "",
   };
-
   const [form, setForm] = useState(initialForm);
   const [exito, setExito] = useState(false);
-
-  const order = {
-    buyer: form,
-    items: [...productosAgregados],
-    total: totalPrice,
-  };
+  const [orderId, setOrderId] = useState(undefined);
+  const [orden, setOrden] = useState(null);
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    const order = {
+      buyer: form,
+      items: [...productosAgregados],
+      total: totalPrice,
+    };
+    setOrden(order);
+  }, [form, totalPrice]);
 
   const handleChange = (e) => {
     setForm({
@@ -37,83 +50,130 @@ export default function Checkout() {
   };
   const handleSubmit = (e) => {
     const db = getFirestore();
-    setExito(true);
+
     const coleccion = collection(db, "orders");
     e.preventDefault();
-    addDoc(coleccion, order);
+    addDoc(coleccion, orden).then(({ id }) => setOrderId(id));
+    setExito(true);
     let orderDoc;
 
-    order.items.map((item) => {
+    orden.items.map((item) => {
       return (
         (orderDoc = doc(db, "products", item.id)),
-        updateDoc(orderDoc, { stock: item.stock - item.quantity }),
-        clear()
+        updateDoc(orderDoc, { stock: item.stock - item.quantity })
       );
     });
+    activeLoading();
+    clear();
+  };
+
+  const activeLoading = () => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 1500);
   };
 
   return (
     <>
-      {exito ? (
-        <Stack sx={{ width: "100%" }}>
-          <Alert severity="success">"Compra realizada con éxito!"</Alert>
-          <Button size="large" variant="contained" sx={{ m: "0 auto", mt: 2 }}>
-            <NavLink
-              to="/"
-              style={() => ({
-                width: "100%",
-                color: "white",
-                textDecoration: "none",
-              })}
-            >
-              Volver
-            </NavLink>
-          </Button>
-        </Stack>
-      ) : (
-        <Box
-          component="form"
-          sx={{
-            "& .MuiTextField-root": { m: 4, width: "25ch" },
-          }}
-          noValidate
-          autoComplete="off"
-          onSubmit={handleSubmit}
+      {loading ? (
+        <Backdrop
+          sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
+          open={loading}
         >
-          <div>
-            <TextField
-              required
-              name="name"
-              label="Nombre"
-              onChange={handleChange}
-              value={form.name}
-            />
-            <TextField
-              required
-              name="email"
-              label="Email"
-              onChange={handleChange}
-              value={form.email}
-            />
-            <TextField
-              required
-              name="phone"
-              label="Teléfono"
-              onChange={handleChange}
-              value={form.phone}
-            />
-          </div>
-          <Button
-            type="submit"
-            variant="contained"
+          <CircularProgress color="inherit" />
+        </Backdrop>
+      ) : productosAgregados.length > 0 || orderId !== undefined ? (
+        exito && orden ? (
+          <>
+            <Stack sx={{ width: "100%" }}>
+              <Alert severity="success">Compra realizada con éxito!</Alert>
+              <Paper
+                sx={{ m: "0 auto", mt: "30px", width: "50%", p: "20px" }}
+                elevation={5}
+              >
+                <h2>Datos del cliente: </h2>
+                <p>ID de orden: {orderId}</p>
+                <p>El nombre del comprador es: {orden.buyer.name}</p>
+                <h2>Detalle de productos: </h2>
+                {orden.items.map((item) => (
+                  <>
+                    <p key={item.id}>
+                      {item.title} x {item.quantity}
+                    </p>
+                  </>
+                ))}
+                <h2>Total:</h2>
+                <p>${totalPrice}</p>
+              </Paper>
+
+              <Button
+                size="large"
+                variant="contained"
+                sx={{ m: "0 auto", mt: 2 }}
+              >
+                <NavLink
+                  to="/"
+                  style={() => ({
+                    width: "100%",
+                    color: "white",
+                    textDecoration: "none",
+                  })}
+                >
+                  Volver
+                </NavLink>
+              </Button>
+            </Stack>
+          </>
+        ) : (
+          <Box
+            component="form"
             sx={{
-              m: 4,
-              backgroundColor: "#1976d2",
+              "& .MuiTextField-root": { m: 4, width: "25ch" },
             }}
+            noValidate
+            autoComplete="off"
+            onSubmit={handleSubmit}
           >
-            Comprar
-          </Button>
-        </Box>
+            <div>
+              <TextField
+                required
+                name="name"
+                label="Nombre"
+                onChange={handleChange}
+                value={form.name}
+              />
+              <TextField
+                required
+                name="email"
+                label="Email"
+                onChange={handleChange}
+                value={form.email}
+              />
+              <TextField
+                required
+                name="phone"
+                label="Teléfono"
+                onChange={handleChange}
+                value={form.phone}
+              />
+            </div>
+            <Button
+              type="submit"
+              variant="contained"
+              sx={{
+                m: 4,
+                backgroundColor: "#1976d2",
+              }}
+            >
+              Comprar
+            </Button>
+          </Box>
+        )
+      ) : (
+        <>
+          <Alert severity="error">No tiene productos en el carrito</Alert>
+        </>
       )}
     </>
   );
